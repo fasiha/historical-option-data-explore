b211cbd6c082b7e09cddb225841b552dc9898668
Ahmed Fasih <wuzzyview@gmail.com>
2020-03-10 02:21:20 -0400

    Parse CSV to SQL in TS

--- demo.ts
+++ demo.ts
@@ -1,8 +1,30 @@
-// import * as lib from './lib';
-var lib = require('./lib');
-var CSV_FILE = '200805.csv';
-var HEADERS =
-    'UnderlyingSymbol,UnderlyingPrice,Exchange,OptionSymbol,Type,Expiration,DataDate,Strike,Last,Bid,Ask,Volume,OpenInterest,OI2,IV,G1,G2,G3,G4,G5,G6,Alias'
-        .split(',');
+import * as lib from './lib';
+// var lib = require('./lib');
+var CSV_FILE = 'SPYJan2018-Feb2020.csv';
 
-var arr = lib.loadCsv(CSV_FILE, HEADERS);
+// underlying,underlying_last,optionroot,type,expiration,quotedate,strike,last,bid,ask,volume,openinterest,impliedvol,delta,gamma,theta,vega,IVBid,IVAsk
+
+var WANTED =
+    'underlying,underlying_last,optionroot,type,expiration,quotedate,strike,last,bid,ask,volume,openinterest,impliedvol,delta,gamma,theta,vega,IVBid,IVAsk'
+        .split(',');
+var NUMCOLS =
+    'underlying_last,strike,last,bid,ask,volume,openinterest,impliedvol,delta,gamma,theta,vega,IVBid,IVAsk'.split(',');
+var DATECOLS = 'expiration,quotedate'.split(',');
+var arr = lib.loadCsv(CSV_FILE, WANTED, NUMCOLS, DATECOLS);
+
+function tosql(arr: lib.Csv) {
+  console.log('-- ok!');
+  const cols = arr.cols.join(',');
+  for (const row of arr.rows) {
+    let [underlying, underlying_last, optionroot, type, expiration, quotedate, strike, last, bid, ask, volume,
+         openinterest, impliedvol, delta, gamma, theta, vega, IVBid, IVAsk] = row;
+    let fixdate = (d: any) => `"${d.toISOString()}"`;
+    const vals = `"${underlying}", ${underlying_last}, "${optionroot}", "${type}", ${fixdate(expiration)}, ${
+        fixdate(quotedate)}, ${strike}, ${last}, ${bid}, ${ask}, ${volume}, ${openinterest}, ${impliedvol}, ${delta}, ${
+        gamma}, ${theta}, ${vega}, ${IVBid}, ${IVAsk}`;
+    const cmd = `insert into spy (${cols}) values (${vals});`;
+    console.log(cmd);
+  }
+}
+
+tosql(arr);
--- lib.ts
+++ lib.ts
@@ -1,9 +1,8 @@
 import assert from 'assert';
 import * as fs from 'fs';
 
-const numRe = /^-?[.0-9]+$/;
 const dateRe = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-function MMDDYYYYSlashToDate(s: string) {
+function MMDDYYYYSlashToDate(s: string): Date {
   const hit = s.match(dateRe);
   if (hit) {
     const mm = +hit[1];
@@ -11,22 +10,63 @@ function MMDDYYYYSlashToDate(s: string) {
     const dd = +hit[2];
     assert(dd >= 1 && dd <= 31);
     const yyyy = +hit[3];
-    assert(yyyy >= 1985 && yyyy <= 3000);
+    assert(yyyy >= 1970 && yyyy <= 2120);
     return new Date(Date.UTC(yyyy, mm - 1, dd));
   }
-  return undefined;
+  throw new Error('not MM/DD/YYYY ' + s);
 }
 
-export function loadCsv(fname: string, header: string[]) {
-  return fs.readFileSync(fname, 'utf8').trim().split('\n').map((o, lino) => {
-    const arr = o.trim().split(',');
-    if (arr.length !== header.length) { throw new Error('uneven lengths @ ' + lino); }
-    const ret: {[key: string]: any} = {};
-    for (const [idx, key] of header.entries()) {
-      const x = arr[idx];
-      const parsed = parseFloat(x);
-      ret[key] = (x.match(numRe) && !isNaN(parsed)) ? parsed : (MMDDYYYYSlashToDate(x) || x);
+export type Row = (string|number|Date)[];
+export interface Csv {
+  rows: Row[];
+  cols: string[]
+}
+export function loadCsv(fname: string, wantedCols: string[], numberCols: string[], dateCols: string[]): Csv {
+  let lines = fs.readFileSync(fname, 'utf8').trim().split('\n');
+  let header = lines[0].split(',').map(s => s.trim());
+
+  {
+    let check = (v: string[], msg: string) => assert(v.every(col => header.indexOf(col) >= 0), msg);
+    check(wantedCols, 'wanted column(s) not found');
+    check(numberCols, 'number column(s) not found');
+    check(dateCols, 'date column(s) not found');
+    // dates and numbers are also in wanted
+    let w = new Set(wantedCols);
+    assert(numberCols.every(x => w.has(x)));
+    assert(dateCols.every(x => w.has(x)));
+    // no overlap
+    let n = new Set(numberCols);
+    let d = new Set(dateCols);
+    assert(numberCols.every(x => !d.has(x)));
+    assert(dateCols.every(x => !n.has(x)));
+  }
+
+  let wantedIdx: number[] = [];
+  let numIdx: number[] = [];
+  let dateIdx: number[] = [];
+  {
+    let col2idx = new Map(header.map((col, i) => [col, i]));
+    wantedIdx = wantedCols.map(col => col2idx.get(col) ?? -1);
+    numIdx = numberCols.map(c => col2idx.get(c) ?? -1);
+    dateIdx = dateCols.map(c => col2idx.get(c) ?? -1);
+    assert(wantedIdx.every(n => n >= 0));
+    assert(numIdx.every(n => n >= 0));
+    assert(dateIdx.every(n => n >= 0));
+  }
+
+  const processRow = (raw: string, id: number) => {
+    let v: (string|number|Date)[] = raw.split(',');
+    numIdx.forEach(i => v[i] = parseFloat(v[i] as string));
+    try {
+      dateIdx.forEach(i => v[i] = MMDDYYYYSlashToDate(v[i] as string));
+    } catch (err) {
+      console.error(`error in ${id}`, {numIdx, dateIdx, raw, v, header});
+      throw err;
     }
-    return ret;
-  });
+    return wantedIdx.map(i => v[i]);
+  };
+
+  // lines might be very long so I don't want to unshift it nor do I want to append potentially millions of times to an
+  // array. So I use this Array.from approach.
+  return {cols: wantedCols, rows: Array.from(Array(lines.length - 1), (_, i) => processRow(lines[i + 1], i + 1))};
 }
